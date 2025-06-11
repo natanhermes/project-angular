5933ff987042debde6279991f6b7cb21cb348f1d
Natã Hermes <nhermesdev@gmail.com>
2025-06-11 17:46:09 -0300

    feat: add proxy configuration and environment settings for API integration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://api-publica.datajud.cnj.jus.br',
+    apiKey: 'ApiKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw==',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api-publica.datajud.cnj.jus.br',
+    apiKey: 'ApiKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw==',
+};
